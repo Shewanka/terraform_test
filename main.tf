06414cb58accbd00d58e41fcc779878e7a9bfa96
Shewanka <shewankajain@gmail.com>
2025-09-29 11:38:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "example" {
- ami = "ami-0a716d3f3b16d290c" # Use the appropriate
-AMI ID for your region
+ ami = "ami-0a716d3f3b16d290c" 
  instance_type = var.instance_type
  tags = {
  Name = "ExampleInstance"
  }
 
 }
+
