d7786a0aeb0e051b6937658397b2e5653c9e9640
Shewanka <shewankajain@gmail.com>
2025-09-29 11:34:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
  description = "AWS region to deploy infrastructure"
  type = string
- default = "us-east-1"
+ default = "eu-north-1"
 }
 variable "instance_type" {
  description = "Instance type for EC2"
  type = string
- default = "t2.micro"
+ default = "t3.micro"
 }
